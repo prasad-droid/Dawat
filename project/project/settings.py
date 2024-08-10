6d3874dcfea087327cb5dcdacf626f205cabe3ad
Chandanking14 <vr5544430@gmail.com>
2024-08-10 15:16:16 +0530

    done

--- project/project/settings.py
+++ project/project/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for project project.
 
@@ -117,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
